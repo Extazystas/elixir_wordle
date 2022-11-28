a60f75e2fe514f0007ab253da532fa493993065b
extazystas <2153311+Extazystas@users.noreply.github.com>
2022-11-28 23:59:43 +0200

    Update call method

--- letter_checker.exs
+++ letter_checker.exs
@@ -1,9 +1,19 @@
 defmodule LetterChecker do
-  def call(guess, secret, letters) do
-    cond do
-      guess == secret -> {:correct, to_string([guess])}
-      guess in letters -> {:partial, to_string([guess])}
-      true -> {:incorrect, to_string([guess])}
-    end
+  def call(guess, secret) do
+    guess
+    |> correct_pass(secret)
+    |> partial_pass(guess)
+  end
+
+  def correct_pass(guess, secret_letters) do
+    secret_letter_charlist = String.to_charlist(secret_letters)
+
+    {result, remainders} =
+      guess
+      |> Enum.zip(initial_state(secret_letter_charlist))
+      |> Enum.reduce({[], secret_letter_charlist}), fn {guess_letter, {_status, secret_letter}},
+                                                       {result, remaining_letters} ->
+                                                        compare_letters()
+                                                       end
   end
 end
