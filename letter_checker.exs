45506704180624c5ea7d9c0ec24790920ec4ea9c
extazystas <2153311+Extazystas@users.noreply.github.com>
2022-11-29 00:26:58 +0200

    Add partial pass and match case

--- letter_checker.exs
+++ letter_checker.exs
@@ -11,9 +11,41 @@ defmodule LetterChecker do
     {result, remainders} =
       guess
       |> Enum.zip(initial_state(secret_letter_charlist))
-      |> Enum.reduce({[], secret_letter_charlist}), fn {guess_letter, {_status, secret_letter}},
+      |> Enum.reduce({[], secret_letter_charlist}, fn {guess_letter, {_status, secret_letter}},
                                                        {result, remaining_letters} ->
-                                                        compare_letters()
-                                                       end
+        compare_letter(guess, secret_letter, result, remaining_letters)
+      end)
+      {Enum.reverse(result), remainders}
+  end
+
+  def compare_letter(guess, _, result, remaining_letters) do
+    {[{:correct, to_string([guess])} | result], remaining_letters -- [guess]}
+  end
+
+  def compare_letter(guess, _, result, remaining_letters) do
+    {[{:incorrect, to_string([guess])} | result], remaining_letters}
+  end
+
+  def partial_pass({letter_state, reminders}, guess) do
+    {result, reminders} =
+      guess
+      |> String.to_charlist()
+      |> Enum.zip(letter_state)
+      |> Enum.reduce({[], remainders}, fn {guess, {status, _secret_letter}},
+                                          {result, remaining_letters} ->
+        partial_match(guess, status, remaining_letters, result)
+      end)
+
+      {Enum.reverse(result), remainders}
+  end
+
+  defp partial_match(guess, _, remaining_letters, result) do
+    cond do
+      Enum.member?(remainders, guess) ->
+        {[{:partial, to_string([guess])} | result], remainders -- [guess]}
+
+      true ->
+        {[{:incorrect, to_string([guess])} | result], remainders}
+    end
   end
 end
